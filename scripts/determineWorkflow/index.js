8ac8c418565f0c61f2885433f7c8b1a51d977fee
Fredrik Høisæther Rasch <fredrik.rasch@arva.no>
2021-08-27 11:15:15 +0200

    Add determineWorkflow step

---
+++ scripts/determineWorkflow/index.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {Object} InputState
+ * @property { import("@octokit/rest").Octokit } github - A pre-authenticated octokit/rest.js client with pagination plugins
+ * @property { import("@actions/github/lib/context").Context } context - An object containing the context of the workflow run
+ * @property { import("@actions/core") } core - A reference to the @actions/core package
+ */
+
+/**
+ * @param {InputState} param0
+ */
+module.exports = async ({github, context, core}) => {
+  const run_resp = await github.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}', {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: context.runId
+  })
+  /** @type {{ workflow_id: Number}} */
+  const { workflow_id } = run_resp
+  core.setOutput('workflow_id', workflow_id)
+}
