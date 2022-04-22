715fc3487b17619837e18e2d6892aa6159710512
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-04-22 17:33:50 +0200

    Move checkout-pr-branch script to gh-actions

--- .github/actions/checkout-pr-branch/index.js
+++ .github/actions/checkout-pr-branch/index.js
@@ -7,42 +7,44 @@
  * }} param0
  * @param {string} branch_name
  */
-module.exports = async ({ github, context, core, exec }, branch_name) => {
-  let git_exitcode = 0
+module.exports = async ({
+  github, context, core, exec,
+}, branch_name) => {
+  let git_exitcode = 0;
   git_exitcode = await exec.exec('git', [
     'checkout',
     '-B',
     branch_name,
-    'HEAD'
-  ])
-  core.info(`Asking GitHub API about ref named ${branch_name}`)
+    'HEAD',
+  ]);
+  core.info(`Asking GitHub API about ref named ${branch_name}`);
   try {
     _ = await github.rest.git.getRef({
       owner: context.repo.owner,
       repo: context.repo.repo,
       ref: `heads/${branch_name}`,
-    })
+    });
   } catch (err) {
     /** @type { import('@octokit/request-error').RequestError } */
-    const reqError = err
-    const { name, status } = reqError
+    const reqError = err;
+    const { name, status } = reqError;
     if (name !== 'HttpError' || status !== 404) {
-      throw err
+      throw err;
     }
-    core.info('git ref not found, no merge necessary')
-    return
+    core.info('git ref not found, no merge necessary');
+    return;
   }
   git_exitcode = await exec.exec('git', [
     'fetch',
-    'origin'
-  ])
-  core.info('git ref found, staging merge')
+    'origin',
+  ]);
+  core.info('git ref found, staging merge');
   git_exitcode = await exec.exec('git', [
     'merge',
     '--no-commit',
     '--allow-unrelated-histories',
     '-s',
     'ours',
-    `origin/${branch_name}`
-  ])
-}
+    `origin/${branch_name}`,
+  ]);
+};
